10796544960f4bd572ce3bd566b9bb66d84edb16
priyanshupande <priyanshupande23@gmail.com>
2024-12-08 21:51:37 +0530

    correaction in gellaryjsx

--- src/TabMenu/Galleryreact.jsx
+++ src/TabMenu/Galleryreact.jsx
@@ -23,11 +23,11 @@ const Galleryreact = () => {
             <hr />
             <div className="menu-tabs container">
                 <div className="menu-tab d-flex justify-content-around">
-                    <button className="btn btn-warning" onClick={() => filterItem('breakfast')}> Breakfast</button>
-                    <button className="btn btn-warning" onClick={() => filterItem('lunch')}> Lunch</button>
-                    <button className="btn btn-warning" onClick={() => filterItem('evening')}> Evening</button>
-                    <button className="btn btn-warning" onClick={() => filterItem('dinner')}> Dinner</button>
-                    <button className="btn btn-warning" onClick={() => filterItem('All')}> All</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('breakfast')}>Breakfast</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('lunch')}>Lunch</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('evening')}>Evening</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('dinner')}>Dinner</button>
+                    <button className="btn btn-warning" onClick={() => filterItem('All')}>All</button>
                 </div>
             </div>
 
@@ -36,40 +36,36 @@ const Galleryreact = () => {
                 <div className="row">
                     <div className="col-11 mx-auto">
                         <div className="row my-5">
-                            {
-                                items.map((elem) => {
-                                    const { id, name, image, description, price } = elem;
+                            {items.map((elem) => {
+                                const { id, name, image, description, price } = elem;
 
-                                    return (
-                                        <div className="item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5" key={id}>
-                                            <div className="row Item-inside">
-                                                {/* Image Section */}
-                                                <div className="col-12 col-md-12 col-lg-4 img-div">
-                                                    <img src={image} alt={name} className="img-fluid" />
+                                return (
+                                    <div className="item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5" key={id}>
+                                        <div className="row Item-inside">
+                                            {/* Image Section */}
+                                            <div className="col-12 col-md-12 col-lg-4 img-div">
+                                                <img src={image} alt={name} className="img-fluid" />
+                                            </div>
+                                            {/* Menu description */}
+                                            <div className="col-12 col-md-12 col-lg-8">
+                                                <div className="main-title pt-4 pb-3">
+                                                    <h1>{name}</h1>
+                                                    <p>{description}</p>
                                                 </div>
-                                                {/* Menu description */}
-                                                <div className="col-12 col-md-12 col-lg-8">
-                                                    <div className="main-title pt-4 pb-3">
-                                                        <h1>{name}</h1>
-                                                        <p>{description}</p>
+                                                <div className="menu-price-book">
+                                                    <div className="price-book-divide d-flex justify-content-between">
+                                                        <h2>Price: {price}</h2>
+                                                        <button className="btn btn-primary" onClick={() => alert('Order placed!')}>
+                                                            Order Now
+                                                        </button>
                                                     </div>
-                                                    <div className="menu-price-book">
-                                                        <div className="price-book-divide d-flex justify-content-between">
-                                                            <h2>Price: {price}</h2>
-                                                            <button className="btn btn-primary" onClick={() => alert('Order placed!')}>
-                                                                Order Now
-                                                            </button>
-
-                                                        </div>
-                                                        <p>*Prices may vary on selected date</p>
-                                                    </div>
-
+                                                    <p>*Prices may vary on selected date</p>
                                                 </div>
                                             </div>
                                         </div>
-                                    );
-                                })
-                            }
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
